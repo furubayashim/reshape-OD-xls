81a1d08b1a36e4ed7e8227cd30c63eae1bca36bc
mfurubayashi <maiko.furubayashi@gmail.com>
2020-06-18 16:07:01 +0900

    made it usable for different ns

--- reshape-OD-xls.py
+++ reshape-OD-xls.py
@@ -3,30 +3,36 @@
 import pandas as pd
 import numpy as np
 import sys
+import os
 
 if len(sys.argv) >1:
     samplefile = sys.argv[1]
 else:
     print("enter file name")
 
-df = pd.read_excel(samplefile,skiprows=35)
-mean = df.iloc[0,1:]
-stdev = df.iloc[1,1:]
+output_folder = 'xlsx/'
+if not os.path.exists(output_folder):os.makedirs(output_folder)
 
-mean_op = pd.DataFrame()
-stdev_op = pd.DataFrame()
+df = pd.read_excel(samplefile)
 
-for a in ['A','B','C','D','E','F','G','H']:
-    d = mean.filter(regex=a)
-    d.name = a
-    d.index = np.arange(1,13,1)
-    mean_op = mean_op.append(d)
+# find "Well" in the sheet and remake data
+data_index = (df.iloc[:,0] == 'Well').idxmax()
+data_df = df.iloc[data_start+1:-2,1:]
+well_label = df.iloc[data_index,1:].values
+col_label = df.iloc[data_start+1:-2,0].values
+data_df.columns = well_label
+data_df.index = col_label
+data_df
 
-for a in ['A','B','C','D','E','F','G','H']:
-    d = stdev.filter(regex=a)
-    d.name = a
-    d.index = np.arange(1,13,1)
-    stdev_op = stdev_op.append(d)
+def convert_to_96_sheet(series):
+    outdf = pd.DataFrame()
+    for a in ['A','B','C','D','E','F','G','H']:
+        d = series.filter(regex=a)
+        d.name = a
+        d.index = np.arange(1,13,1)
+        outdf = outdf.append(d)
+    outdf.to_excel('{}OD_{}.xlsx'.format(output_folder,series.name))
+    return outdf
 
-mean_op.to_excel('OD.xlsx')
-stdev_op.to_excel('OD_stdev.xlsx')
+for name,series in data_df.iterrows():
+    convert_to_96_sheet(series)
